9f1bd3f224a5a2cb314f65a4da4baea30e55efba
haruharu <haruka373000@gmail.com>
2020-11-09 18:08:34 +0900

    画像投稿用ジェムインストール

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :status_id, :shipping_cost_id, :prefectures_id, :days_to_ship_id, :price, :user)
   end
 end
